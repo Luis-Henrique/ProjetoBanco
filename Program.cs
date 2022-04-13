772f1547adb76d9fcd33d91e56b2be824c96bd46
Luis Henrique <henriqueluis3104@hotmail.com>
2022-04-13 08:47:28 -0300

    Commit inicial

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Conta c1 = new Conta(1);
+Conta c2 = new Conta(2);
+
+Console.WriteLine("" + c1.Codigo);
+
+c1.Depositar(1000.00);
+c1.Sacar(200.00);
+
+Console.WriteLine("" + c1.Saldo);
+
+c2.Depositar(200.00);
+c2.Sacar(150.00);
+
+Console.WriteLine("" + c2.Saldo);
+
+c1.Transferir(10000.00, c2);
+
+Console.WriteLine("" + c1.Saldo);
+Console.WriteLine("" + c2.Saldo);
+
